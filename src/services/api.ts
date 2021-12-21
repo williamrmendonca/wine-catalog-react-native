7345f1c6615e22700ca59ec9b9671e22f989b2c6
William Mendonça <william@tecmasternet.com.br>
2021-12-21 09:00:20 -0300

    feat: :sparkles: config environment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: process.env.DB_HOST,
+    headers: {
+        'Content-Type': 'application/json'
+      }
 });
-
+  
 export default api;
